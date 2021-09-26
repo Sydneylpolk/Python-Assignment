d3e1928f4d4b45cd91e2b3ef7588e53ba72f0943
Sydneylpolk <90217847+Sydneylpolk@users.noreply.github.com>
2021-09-26 14:50:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,27 @@ result={
 
 file = open("python.txt")
 
+months_done = []
 for line in file:
    if(len(line)>=56):
        result["total_requests"]+=1
        data=line.split()
        date = data[3][1::].split(':')
+         
+month = date[0][3::]
+if month not in months_done:
+     file_name = month[:3:]+month[4::]
+     if(len(file_name)) == 7:
+        month_file = open(month[:3:]+month[4::]+".txt",'w')
+print(file_name)
+
+months_done.append(month)
+month_file.write(line)
+
+if month in result["past_6_month"]:
+     result["past_6_month"][month]+=1
+else:
+     result["past_6_month"][month]=0
        
 
 
