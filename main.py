aa013f9ea391abbf78e4eb10bb97368e37307997
Sydneylpolk <90217847+Sydneylpolk@users.noreply.github.com>
2021-10-09 13:55:06 -0500

    Update to code to make view easier to understand

--- main.py
+++ main.py
@@ -9,88 +9,124 @@ with open("python.txt","wb") as textfile:
    #writing one chunk at a time to pdf file
        if chunk:
            textfile.write(chunk)
+            
 #creates a dictionary called result
 result={
-"total_requests":0,
-"per_day":{},
-"per_week":{},
-"per_month":{},
-"not_successful":0,
-"redirected_elsewhere":0,
-"request_frequency":{},
-"most_requested":[0,[]], #max requests/list of all files containing the same number of requests
-"least_requested":[0,[]] #min requests/list of all files containing the smae number of requests.
+  
+"total":0,
+   
+"day":{},
+   
+"week":{},
+   
+"month":{},
+   
+"non_successful":0,
+   
+"redirected":0,
+   
+"frequency":{},
+   
+#max requests/list of all files containing the same number of requests   
+"highest_requested":[0,[]], 
+
+#min requests/list of all files containing the smae number of requests.
+"lowest_requested":[0,[]] 
 }
+
 file = open("python.txt")
 
 date_day = None
+
 days = 0
+
 week = None
+
 months_done = []
+
+
 #creating lists for date_day and day
 #parsing the file for the criteria in the read me. 
 #If the criteria is found in the line, it adds one to the vairable
+
 for line in file:
+   
    if(len(line)>=56):
-       result["total_requests"]+=1
+       result["total"]+=1
        data=line.split()
        date = data[3][1::].split(':')
+         
        if not (date_day == date[0]):
            date_day = date[0]
            days += 1
+         
            if(days%7 == 0):
                week = date_day
-       if date[0] in result["per_day"]:
-           result["per_day"][date[0]]+=1
+               
+       if date[0] in result["day"]:
+           result["day"][date[0]]+=1
        else:
-           result["per_day"][date[0]]=0
+           result["day"][date[0]]=0
       
-       if week in result["per_week"]:
-           result["per_week"][week]+=1
+       if week in result["week"]:
+           result["week"][week]+=1
        else:
-           result["per_week"][week] = 0
+           result["week"][week] = 0
        month = date[0][3::]
+      
        if month not in months_done:
            file_name = month[:3:]+month[4::]
+         
            if(len(file_name)) == 7:
                month_file = open(month[:3:]+month[4::]+".txt",'w')
                print(file_name)
            months_done.append(month)
        month_file.write(line)
-       if month in result["per_month"]:
-           result["per_month"][month]+=1
+      
+       if month in result["month"]:
+           result["month"][month]+=1
        else:
-           result["per_month"][month]=0
+           result["month"][month]=0
+            
         #this code checks to see if the number 4 or 3 are in the code area to decide if it has been redirected or not successful. Ex: Error 404 (not_successful)
        if data[-2][0]=="4":
-           result["not_successful"]+=1
+           result["non_successful"]+=1
+         
        if data[-2][0]=="3":
-           result["redirected_elsewhere"]+=1
-       if data[6] in result["request_frequency"]:
-           result["request_frequency"][data[6]]+=1
+           result["redirected"]+=1
+            
+       if data[6] in result["frequency"]:
+           result["frequency"][data[6]]+=1
        else:
-           result["request_frequency"][data[6]]=1
+           result["frequency"][data[6]]=1
 
 #This code is used to creat the most/least frequent requested files.           
-maxm=result["request_frequency"]["index.html"]
-minm=result["request_frequency"]["index.html"]
+maxm=result["frequency"]["index.html"]
+
+minm=result["frequency"]["index.html"]
+
 maxlist=["index.html"]
+
 minlist=["index.html"]
-for i in result["request_frequency"]:
-   if result["request_frequency"][i] > maxm:
-       maxm = result["request_frequency"][i]
+
+for i in result["frequency"]:
+   
+   if result["frequency"][i] > maxm:
+       maxm = result["frequency"][i]
        maxlist=[i]
-   if result["request_frequency"][i] < minm:
-       minm = result["request_frequency"][i]
+      
+   if result["frequency"][i] < minm:
+       minm = result["frequency"][i]
        minlist=[i]
   
-   if result["request_frequency"][i] == maxm:
+   if result["frequency"][i] == maxm:
        maxlist.append(i)
   
-   if result["request_frequency"][i] == minm:
+   if result["frequency"][i] == minm:
        minlist.append(i)
 
-result["most_requested"]=[maxm,maxlist]
-result["least_requested"]=[minm,minlist]
+result["highest_requested"]=[maxm,maxlist]
+
+result["lowest_requested"]=[minm,minlist]
 
 print(result)
